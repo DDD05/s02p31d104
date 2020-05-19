6ee3d9ee3b5d07e60f08dac8022b22545d752c2e
이옥주 <dhrwnmc@gmail.com>
2020-05-19 17:17:50 +0900

    add writeBoard page

---
+++ front-end/src/components/writeBoard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-content>
+    <v-container>
+      <form>
+        <br />
+        <v-text-field v-model="title" label="제목" required></v-text-field>
+        <br /><br /><br />
+
+        <v-textarea solo counter v-model="content" label="자유롭게 기록하세요!"></v-textarea>
+        <v-btn @click="clear()">clear</v-btn> &nbsp;&nbsp;
+        <v-btn @click="submit()">submit</v-btn>
+      </form>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: "",
+    content: "",
+  }),
+
+  methods: {
+    submit() {
+      console.log("글쓰기!!");
+      alert("글쓰기!!");
+    },
+    clear() {
+      this.content = "";
+    },
+  },
+};
+</script>
